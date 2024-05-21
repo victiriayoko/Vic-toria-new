a594f1161a273e3d673be4612b52fc74761d1f75
victoriayoko <132665912+victiriayoko@users.noreply.github.com>
2024-05-20 21:38:18 -0300

    Add files via upload

--- Vic-toria-new-main/Vic-toria-new-97a77163563b338c6e0c5ebeda16be6a58079e50/pagina2/menu2.js
+++ Vic-toria-new-main/Vic-toria-new-97a77163563b338c6e0c5ebeda16be6a58079e50/pagina2/menu2.js
@@ -1,8 +1,4 @@
 window.addEventListener("scroll",function(){
-    let header = document.querySelector ('.cabeçalho')
-    header.classList.toggle('rolagem',window.scrollY > 0)
-})
-window.addEventListener("scroll",function(){
     let header = document.querySelector ('.cabeçalho-dois')
     header.classList.toggle('rolar',window.scrollY > 0)
 })
